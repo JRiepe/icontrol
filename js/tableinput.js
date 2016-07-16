fe12aa1685fbf12269ec266b8eb83ad12ecccb54
James Kemp <jameskemp2796@yahoo.com>
2016-07-16 15:20:32 -0400

    added some javascript and changed table layout

--- js/tableinput.js
+++ js/tableinput.js
@@ -1,4 +1,14 @@
 //psudocode and stuff
+//grabs the data that was sent to firebase and displays it in the table
+//has an option to delete each row
+
+//lets break this into steps 
+
+//once the user hits submit, their information is sent into the databse.
+//the user data is inputed into s generated row if the user successfully unputted their data without errors
+//a new tr(table row) needs to be generated with a unique id so it can be deleted if need be
+//once the user clicks the delete button in the row, it needs to delete that row by using the row id
+//the user can add as many rows as they want but it needs to be linked to their unique user id
 
 
 //on ready function
@@ -19,8 +29,10 @@ $(document).ready(function(){
 			var invStock = $("#inStockInput").val().trim();
 			var invMRP =$("#mRPInput").val().trim();
 
+			
+
 		} //ends sendRowButton onclick
 
 	}; //ends iControlTable function
 
-}); //ends document.onready function
+}); //ends document.ready function
