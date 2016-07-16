c40b08320492c81a3b83bcfe79a784497677d08b
sheli16 <sheli16@live.seminolestate.edu>
2016-07-16 01:07:19 -0400

    user login with firebase, popup window, styling and size adjustment to be done

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+window.onload();
+
+$('#login').on('click', function(){
+	 myFunction();
+})
+
+function myFunction() {
+    var myWindow = window.open("../html/login.html", "", "width=100","height=100");
+}
+
+function myFunction2() {
+    var myWindow2 = window.close()
+}
