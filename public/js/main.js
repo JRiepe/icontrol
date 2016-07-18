d412ee83ca5e6d09d6860d04eb47cca02e1b4e0d
sheli16 <sheli16@live.seminolestate.edu>
2016-07-18 18:59:56 -0400

    working on login object connecting to DB, js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+var logSignInfo = require('../html/login.html');
+
 window.onload();
 
 $('#login').on('click', function(){
@@ -11,3 +13,58 @@ function myFunction() {
 function myFunction2() {
     var myWindow2 = window.close()
 }
+// -----code pulled from uer.js file from the passport example from ari-------------------------------------------------------------------------
+
+function User (userObj) {
+	logSignInfo.providerdata.uid = userObj.username
+	console.log(providerdata)
+	// this.username = userObj.username
+	// this.password = userObj.password
+}
+
+module.exports = User
+
+module.exports.saveUser = function(userObj, callback){
+	logSignInfo.addUserToDB(userObj, function(status, err){
+		if (err) return callback(false);
+		callback(true);
+	});
+}
+// -----code pulled from orm.js file from passport example from ari------------------------------------------------------------------------------------------
+// not yet adjusted
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: process.argv[2],
+	database: 'dbUsers'
+});
+
+function connectToDB(){
+	connection.connect(function(err){
+		if (err) {
+			console.error('error connection:', err.stack);
+			return
+		}
+		console.log('connected to MySQL DB')
+	});
+}
+
+module.exports.connectToDB = connectToDB;
+
+function addUserToDB(userObj, callback){
+	connection.query('INSERT INTO tblUsers SET ?', userObj, function(err, results){
+		if (err) return callback(false, err)
+		callback(true. null)
+	});
+}
+
+module.exports.addUserToDB = addUserToDB;
+
+function findUser(username, callback){
+	connection.query('SELECT * FROM tblUsers WHERE ?', {username: username}, function(err, user){
+		callback(err, user)
+	})
+}
+module.exports.findUser = findUser;
